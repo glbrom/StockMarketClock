b0d72c967acb9bfd5f0667fcad0a9964eb077e2c
glbrom <golbrm@gmail.com>
2023-08-27 15:15:02 +0300

    no message

---
+++ MarketClock/MarketClock/MarketClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketClockApp.swift
+//  MarketClock
+//
+//  Created by Macbook on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MarketClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MarketClockFace()
+        }
+    }
+}
